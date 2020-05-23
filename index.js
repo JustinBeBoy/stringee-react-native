92b16df6344e8c62c02188d8adfd72395d223aca
JustinDo <justin.namdo@gmail.com>
2020-05-23 21:37:25 +0700

    add custom

--- index.js
+++ index.js
@@ -2,5 +2,7 @@ import StringeeClient from "./src/StringeeClient";
 import StringeeCall from "./src/StringeeCall";
 import StringeeVideoView from "./src/StringeeVideoView";
 import StringeeRoom from "./src/StringeeRoom";
+import StringeeCallCustoms from "./src/StringeeCallCustoms";
+import StringeeClientCustoms from "./src/StringeeClientCustoms";
 
-export { StringeeClient, StringeeCall, StringeeVideoView, StringeeRoom };
+export { StringeeClient, StringeeCall, StringeeVideoView, StringeeRoom, StringeeCallCustoms, StringeeClientCustoms };
